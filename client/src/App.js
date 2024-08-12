208706ead0f980abeb4109dba042dc412f2e46f3
Tinu Jos K <k.tinujos@gmail.com>
2024-08-11 21:37:47 -0400

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import EmployeeDirectory from './components/EmployeeDirectory';
